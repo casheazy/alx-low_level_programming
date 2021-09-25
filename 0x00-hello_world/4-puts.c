84e3b5b0ae66bea462af573410e0782b02ebf2d7
Casheazy <abayomi.oshinowo@yahoo.com>
2021-09-25 16:02:54 -0700

    Using function puts in C to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
-int main(void)
+main()
 {
-char str[] = "Programming is like building a multilingual puzzle\n";
+char str[] = "\"Programming is like building a multilingual puzzle\n";
 puts(str);
 return (0);
 }
