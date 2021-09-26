a0c6c4d7d3ce35c945c4c958eb070195a29a593c
Casheazy <abayomi.oshinowo@yahoo.com>
2021-09-25 18:12:52 -0700

    Using printf in C to print out a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-prints a string using printf
+ *
+ * Return: returns value of 0 if successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the  outconme is a piece of art");
+return (0);
+}
