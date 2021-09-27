a0bc852e395395e25f9dc2aa5972bf67a8eafcb6
Casheazy <abayomi.oshinowo@yahoo.com>
2021-09-26 19:24:37 -0700

    Using sizeof unary operator in C to print sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
+/**
+ *main-function using sizeof unary operator to print data type sizes
+ *
+ *Return: returns value of 0
+ */
 int main(void)
 {
-  printf("Size of a char: %d  byte(s)\n", sizeof(char));
-  printf("Size of an int: %d byte(s)\n", sizeof(int));
-  printf("Size of a long int: %d byte(s)\n", sizeof(long));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
-  printf("Size of a float: %d byte(s)\n", sizeof(float));
-  return (0);
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
 }
